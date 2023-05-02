494addc86c95f29d2d77116ca9beaaacb27a7082
Rob Wygand <rob.wygand@lookout.com>
2023-05-02 15:04:55 -0700

    make a little icon appear

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("icon.png"),
+        ..default()
+    });
 }
