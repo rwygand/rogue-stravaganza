546a0a4c3fef0a8ed57f4d214cf8686fe35d7db6
Rob Wygand <rob.wygand@lookout.com>
2023-05-02 17:44:13 -0700

    Render a tilemap with some controls

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,78 @@
 use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+
+    let texture_handle: Handle<Image> = asset_server.load("tiles.png");
+
+    commands.spawn(Camera2dBundle::default());
+
+    let map_size = TilemapSize { x: 32, y: 32 };
+    let tilemap_entity = commands.spawn_empty().id();
+    let mut tile_storage = TileStorage::empty(map_size);
+    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
+
+    fill_tilemap(TileTextureIndex(1),
+                 map_size,
+                 TilemapId(tilemap_entity),
+                 &mut commands,
+                 &mut tile_storage);
+
+    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
+    let grid_size = tile_size.into();
+    let map_type = TilemapType::default();
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type,
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(texture_handle),
+        tile_size,
+        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
+        ..Default::default()
+    });
+}
+
+fn swap_texture_or_hide(
+    asset_server: Res<AssetServer>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut TilemapTexture, &mut Visibility)>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        let texture_a = TilemapTexture::Single(asset_server.load("tiles.png"));
+        let texture_b = TilemapTexture::Single(asset_server.load("tiles2.png"));
+        for (mut tilemap_tex, _) in &mut query {
+            if *tilemap_tex == texture_a {
+                *tilemap_tex = texture_b.clone();
+            } else {
+                *tilemap_tex = texture_a.clone();
+            }
+        }
+    }
+    if keyboard_input.just_pressed(KeyCode::H) {
+        for (_, mut visibility) in &mut query {
+            *visibility = match *visibility {
+                Visibility::Inherited | Visibility::Visible => Visibility::Hidden,
+                Visibility::Hidden => Visibility::Visible,
+            };
+        }
+    }
+}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: String::from(
+                    "Basic Example - Press Space to change Texture and H to show/hide tilemap.",
+                ),
+                ..Default::default()
+            }),
+            ..default()
+        }).set(ImagePlugin::default_nearest()))
+        .add_plugin(TilemapPlugin)
         .add_startup_system(setup)
+        .add_system(swap_texture_or_hide)
         .run();
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("icon.png"),
-        ..default()
-    });
 }
