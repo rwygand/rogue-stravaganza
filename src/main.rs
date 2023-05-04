c024a2ea52553ca065262306a32ca498ac2f3fe9
Rob Wygand <rob.wygand@lookout.com>
2023-05-03 17:45:33 -0700

    Basic implementation of a game

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,45 @@
 use bevy::prelude::*;
-use bevy_ecs_tilemap::prelude::*;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let texture_handle: Handle<Image> = asset_server.load("tiles.png");
-
-    commands.spawn(Camera2dBundle::default());
-
-    let map_size = TilemapSize { x: 32, y: 32 };
-    let tilemap_entity = commands.spawn_empty().id();
-    let mut tile_storage = TileStorage::empty(map_size);
-
-    fill_tilemap(
-        TileTextureIndex(1),
-        map_size,
-        TilemapId(tilemap_entity),
-        &mut commands,
-        &mut tile_storage,
-    );
-
-    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
-    let grid_size = tile_size.into();
-    let map_type = TilemapType::default();
-
-    commands.entity(tilemap_entity).insert(TilemapBundle {
-        grid_size,
-        map_type,
-        size: map_size,
-        storage: tile_storage,
-        texture: TilemapTexture::Single(texture_handle),
-        tile_size,
-        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
-        ..Default::default()
-    });
-}
-
-fn swap_texture_or_hide(
-    asset_server: Res<AssetServer>,
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut TilemapTexture, &mut Visibility)>,
-) {
-    if keyboard_input.just_pressed(KeyCode::Space) {
-        let texture_a = TilemapTexture::Single(asset_server.load("tiles.png"));
-        let texture_b = TilemapTexture::Single(asset_server.load("tiles2.png"));
-        for (mut tilemap_tex, _) in &mut query {
-            if *tilemap_tex == texture_a {
-                *tilemap_tex = texture_b.clone();
-            } else {
-                *tilemap_tex = texture_a.clone();
-            }
-        }
-    }
-    if keyboard_input.just_pressed(KeyCode::H) {
-        for (_, mut visibility) in &mut query {
-            *visibility = match *visibility {
-                Visibility::Inherited | Visibility::Visible => Visibility::Hidden,
-                Visibility::Hidden => Visibility::Visible,
-            };
-        }
-    }
-}
+mod actions;
+mod assets;
+mod board;
+mod camera;
+mod globals;
+mod graphics;
+mod input;
+mod manager;
+mod pieces;
+mod player;
+mod states;
+mod vectors;
 
 fn main() {
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: String::from(
-                    "Basic Example - Press Space to change Texture and H to show/hide tilemap.",
-                ),
-                ..Default::default()
-            }),
-            ..default()
-        }).set(ImagePlugin::default_nearest()))
-        .add_plugin(TilemapPlugin)
-        .add_startup_system(setup)
-        .add_system(swap_texture_or_hide)
-        .run();
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        resolution: (globals::WINDOW_WIDTH, globals::WINDOW_HEIGHT).into(),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .insert_resource(Msaa::Off)
+        .add_state::<states::MainState>()
+        .add_state::<states::GameState>()
+        .add_plugin(actions::ActionsPlugin)
+        .add_plugin(assets::AssetPlugin)
+        .add_plugin(board::BoardPlugin)
+        .add_plugin(graphics::GraphicsPlugin)
+        .add_plugin(input::InputPlugin)
+        .add_plugin(manager::ManagerPlugin)
+        .add_plugin(pieces::PiecesPlugin)
+        .add_plugin(player::PlayerPlugin)
+        .add_startup_system(camera::setup)
+        .run()
 }
---
+++ src/states.rs
@@ -0,0 +1,16 @@
+use bevy::prelude::*;
+
+#[derive(Clone, Debug, Default, Hash, Eq, States, PartialEq)]
+pub enum MainState {
+    #[default]
+    LoadAssets,
+    Game,
+}
+
+#[derive(Clone, Debug, Default, Hash, Eq, States, PartialEq)]
+pub enum GameState {
+    #[default]
+    None,
+    PlayerInput,
+    TurnUpdate,
+}
